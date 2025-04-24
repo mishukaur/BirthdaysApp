3f608fde64d656a3abf4341296dbaff99e4a4e52
Manjot Kaur <manjotk2003@gmail.com>
2025-04-24 02:10:48 -0400

    followed kaitlyn's codealong!

---
+++ BirthdaysApp/Friend.swift
@@ -0,0 +1,18 @@
+//
+//  Friend.swift
+//  BirthdaysApp
+//
+//  Created by Manjot Kaur on 4/24/25.
+//
+
+import Foundation
+
+class Friend{
+    var name: String
+    var birthday: Date
+    
+    init(name: String, birthday: Date){
+        self.name = name
+        self.birthday = birthday
+    }
+}
