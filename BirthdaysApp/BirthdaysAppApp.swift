6249c7710c9239eb0639e298d6c6025c0ed79ce2
Manjot Kaur <manjotk2003@gmail.com>
2025-04-24 01:38:41 -0400

    Initial Commit

---
+++ BirthdaysApp/BirthdaysAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BirthdaysAppApp.swift
+//  BirthdaysApp
+//
+//  Created by Manjot Kaur on 4/24/25.
+//
+
+import SwiftUI
+
+@main
+struct BirthdaysAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
